a1378466a87c06170849c136130c89df133580fd
cheikhouly <49554960+abdoulayecheikhouly@users.noreply.github.com>
2023-01-01 11:59:24 +0000

    graphql

--- Index.js
+++ Index.js
@@ -1,8 +1,34 @@
+const port = 4000
 const express = require('express')
 const cors= require('cors')
 const mongoDbClient= require('./mongoClient')
 const app = express()
-const port = 4000
+
+ 
+//API REST
+const Product = require('./models/product');
+
+app.get('/products/',async(req,res)=>{
+
+  const products= await Product.find({})
+      
+  try {
+    res.send(products)
+  } catch (e) {
+    res.status(500).send(err)
+  }
+})
+app.get('/products/:category',async(req,res)=>{
+
+ const category = req.params.category
+ const products = await Product.find({ category : category})
+      
+  try {
+    res.send(products)
+  } catch (e) {
+    res.status(500).send(err)
+  }
+})
 
 app.use(cors())
 app.get('/', (req, res) => {
